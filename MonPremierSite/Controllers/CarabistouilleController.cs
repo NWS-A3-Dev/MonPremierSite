570d2b0a2bd1484461533b6a91ad00c16d93e58c
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2021-09-24 14:49:59 +0200

    Add project files.

---
+++ MonPremierSite/Controllers/CarabistouilleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MonPremierSite.Models;
+
+namespace MonPremierSite.Controllers
+{
+    [Route("[Controller]/")]
+    public class CarabistouilleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("{id}/{machin}")]
+        public IActionResult Banane(int id, string machin)
+        {
+            return (View("Blog", new TestModel
+            {
+                Id = id,
+                User = machin
+            }));
+        }
+    }
+}
---
+++ MonPremierSite/Models/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonPremierSite.Models
+{
+    public class TestModel
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+    }
+}
